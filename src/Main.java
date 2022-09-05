85a8952862106b211686303d896d36400bebec61
Evgenii Rybalkin <eugeny.rybalkin@gmail.com>
2022-09-05 16:02:56 +0200

    add java homework task 1

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int tiketPrice = 11_657;
+        int chashBack = 1;
+        int bonus = tiketPrice / 20;
+        System.out.println(chashBack * bonus);
+
+
+    }
+}
